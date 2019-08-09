987998e303b7c7d9a85bf4eed6e36d7a0cf91bc9
fabgitlocal <fmiredin7@gmail.com>
2019-08-09 17:36:24 +0200

    style css for the list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 //import TodoItems from "./TodoItems";
 import TodoItems from "./TodoItems";
+import './TodoList.css';
 
 
 class TodoList extends  Component {
