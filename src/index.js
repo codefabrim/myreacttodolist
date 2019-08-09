bbc5692f95a342e13849f64e10f02f9d1c880b2c
fabgitlocal <fmiredin7@gmail.com>
2019-08-09 10:24:14 +0200

    add render with div in element #container

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+var destination = document.querySelector("#container");
+
+
+ReactDOM.render(
+    <div>
+        <p> Hello</p>
+    </div>,
+    destination
+);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
